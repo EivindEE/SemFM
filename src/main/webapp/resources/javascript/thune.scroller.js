a5269f72f57576cf622353eb2640784b5c104439
Torstein Thune <torstein@gmail.com>
2011-10-24 16:44:20 +0200

    Refactoring and bugfixes in thune.scroller.js

--- src/main/webapp/resources/javascript/thune.scroller.js
+++ src/main/webapp/resources/javascript/thune.scroller.js
@@ -22,9 +22,9 @@ $.fn.thuneScroller = function(o) {
 
         speed: 200,
 
-        visible: 2,
+        visible: 3,
         start: 0,
-        scroll:3,
+        scroll:1,
 
         beforeStart: null,
         afterEnd: null
@@ -35,43 +35,53 @@ $.fn.thuneScroller = function(o) {
 
 		// vars
         var running = false;
+		
         var div = $(this), ul = $(".album_list", div), v = o.visible;
         var li = $(".album", ul), itemLength = li.size(), curr = o.start, totalSlides = li.length;
 		
 		// define css for div, ul, etc
         ul.css({ position: "relative"});
-		var liMargins = css(li, 'marginLeft') + css(li, 'marginRight') + 2;
 		
-		var divSize = div[0].offsetWidth-60; 
+		var liMargins = css(li, 'marginLeft') + css(li, 'marginRight') + css(li, 'borderRight') + css(li, 'borderLeft');
+		var divSize = ($(div[0]).innerWidth() - 62); 
 		
-		li.css({ width: ((divSize/v) - (liMargins*v)) });
+		//li.css({ width: ((divSize/v)-(liMargins*v)) });
 		var liSize = li[0].offsetWidth + liMargins;
-
-		/*while(liSize-liMargins < 210 && v > 0) {
-			v--;
-			li.css({ width: ((divSize/v)-(liMargins*v)) });
-			liSize = li[0].offsetWidth + liMargins;
-			
-			console.log(liMargins + " " + divSize + " " + divSize/v + ((divSize/v)-(liMargins*v))*v);
-			console.log('---');
+		div.css({ width: (liSize*o.visible), margin: "0 auto"});
+		/*are the lis to small?
+		if(css(li, 'min-width')) {
+			console.log('LI TO SMALL');
+			while(liSize-liMargins<css(li, 'min-width') && v>0) {
+				v--;
+				console.log(v);
+				li.css({ width: ((divSize/v)-(liMargins*v)) });
+				console.log((divSize/v)-(liMargins*v));
+				liSize = li[0].offsetWidth + liMargins;
+			}
 		}*/
 		
-		
-		
 		// initial hide operation
-		hideUnused(li, curr, v, o.speed);
+		hideUnused(li, curr, v, 0);
 		
 		// check to see if any of the controls should be disabled on launch
-		$( (curr-o.scroll<0 && o.btnPrev) || (curr+o.scroll+o.visible-1 >= totalSlides && o.btnNext) || [] ).addClass("disabled").attr('disabled', 'disabled');
+		$( (curr-o.scroll<0 && o.btnPrev) || (curr+o.scroll+v-1 > totalSlides && o.btnNext) || [] ).addClass("hidden").attr('disabled', 'disabled');
+		
+		if(totalSlides <= o.visible) {
+			$(o.btnPrev).addClass("hidden");
+			$(o.btnNext).addClass("hidden");
+		}
 		
 		// controller press
+		// todo: add to queue
         if(o.btnPrev)
             $(o.btnPrev).click(function() {
+				console.log('prev');
                 return go(curr-o.scroll);
             });
 
         if(o.btnNext)
             $(o.btnNext).click(function() {
+				console.log('next');
                 return go(curr+o.scroll);
             });
 
@@ -79,24 +89,36 @@ $.fn.thuneScroller = function(o) {
 			
             if(!running) {
 				var bleh = ((curr-to)/o.scroll);
+				
                 // If non-circular and to points to first or last, we just return.
                 if(to<0 || to>totalSlides) return;
                     else curr = to;
 
                 running = true;
 				
-				
-				$(ul).delay(o.speed).animate({left: bleh*liSize*o.scroll}, o.speed).animate({left: 0}, 0);
-				//$(ul).fadeOut(o.speed).animate( {left: -0}, o.speed).fadeIn(o.speed);
+				// determine which way to animate
+				if(o.scroll < o.visible) {
+					// hide overflow while changing lis to prevent flickering
+					$(div).css({overflow: "hidden"}).delay(o.speed*8).queue(function(){ 
+						$(this).css({ overflow: "visible" });
+						$(this).dequeue(); 
+					});
+					
+					if(bleh == 1) {  
+						$(ul).delay(o.speed).animate({left: liSize*o.scroll}, o.speed).animate({left: 0}, 0);
+					} else { 
+						$(ul).delay(o.speed).animate({left: liSize*o.scroll}, 0).animate({left: 0}, o.speed);
+					}
+				}
 				
 				// hide divs who are not going to be displayed
 				hideUnused(li, curr, v, o.speed);
-				
+
 				running = false;
 				
                 // Disable buttons when the carousel reaches the last/first, and enable when not
-				$(o.btnPrev + "," + o.btnNext).removeClass("disabled").removeAttr('disabled');
-                $( (curr-o.scroll<0 && o.btnPrev) || (curr+o.scroll+v-1 >= totalSlides && o.btnNext) || [] ).addClass("disabled").attr('disabled', 'disabled');
+				$(o.btnPrev + "," + o.btnNext).removeClass("hidden").removeAttr('disabled');
+                $( (curr-o.scroll<0 && o.btnPrev) || (curr+o.scroll+1 > totalSlides && o.btnNext) || [] ).addClass("hidden").attr('disabled', 'disabled');
             }
             return false;
         };
@@ -112,8 +134,8 @@ function hideUnused(li, curr, v, speed) {
 			  $(this).dequeue(); 
 			});
 		} else {
-			$(this).delay(speed*2).fadeIn(speed).queue(function(){ 
-			  $(this).css({ display: "inline-block" }); 
+			$(this).delay(speed).fadeIn(speed).queue(function(){ 
+			  $(this).css({ display: "block" }); 
 			  $(this).dequeue(); 
 			});
 		}
