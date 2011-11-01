88873cad811b110fa496155b3cd1f221c83c5831
EivindEE <eivind.elseth@gmail.com>
2011-11-01 11:58:12 +0100

    Added methods to the Event and Record Interfaces. Updated the mock classes to implement the new methods.

--- src/main/java/edu/uib/info310/model/Event.java
+++ src/main/java/edu/uib/info310/model/Event.java
@@ -1,5 +1,14 @@
 package edu.uib.info310.model;
 
 public interface Event {
-
+	
+	public String getDate();
+	
+	public String getLocation();
+	
+	public String getVenue();
+	
+	public String getLong();
+	
+	public String getLat();
 }
