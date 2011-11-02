2bae1fd1c97a5e4698b0e90ec098830d15d5645f
EivindEE <eivind.elseth@gmail.com>
2011-11-02 13:16:21 +0100

    Added a logger and updated main method to provide a code example.

--- src/main/java/edu/uib/info310/search/LastFMSearch.java
+++ src/main/java/edu/uib/info310/search/LastFMSearch.java
@@ -8,16 +8,18 @@ import java.io.Reader;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.uib.info310.transformation.XslTransformer;
 
 public class LastFMSearch {
 	private static final String similarArtistRequest = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=";
 	private static final String artistEvents = "http://ws.audioscrobbler.com/2.0/?method=artist.getevents&artist=";
 	private static final String apiKey = "&api_key=a7123248beb0bbcb90a2e3a9ced3bee9";
-	
+	private static final Logger LOGGER = LoggerFactory.getLogger(LastFMSearch.class);
 	
 	public InputStream getArtistEvents (String artist) throws Exception {
-		
 		
         URL lastFMRequest = new URL(artistEvents+artist+apiKey);
         URLConnection lastFMConnection = lastFMRequest.openConnection();
@@ -26,7 +28,6 @@ public class LastFMSearch {
     }
 	
 	public InputStream getSimilarArtist(String artist) throws Exception {
-		
         URL lastFMRequest = new URL(similarArtistRequest+artist+apiKey);
         URLConnection lastFMConnection = lastFMRequest.openConnection();
         		
@@ -34,19 +35,19 @@ public class LastFMSearch {
     }
 	
 	
-//	public static void main(String[] args) throws Exception {
-//		
-//		File xsl = new File("c:/users/john fredrik/dropbox/workshop/SemFM/src/main/resources/XSL/Events.xsl");
-//		XslTransformer transform = new XslTransformer();
-//		
-//		transform.setXml(getContentLastFM(artistEvents, "Metallica"));
-//		transform.setXsl(xsl);
-//		
-//		System.out.println(transform.transform());
-//		
-//		
-//	
-//	}
+	public static void main(String[] args) throws Exception {
+		
+		File xsl = new File("src/main/resources/XSL/Events.xsl");
+		
+		LastFMSearch search = new LastFMSearch();
+		XslTransformer transform = new XslTransformer();
+		
+		transform.setXml(search.getArtistEvents("Metallica"));
+		transform.setXsl(xsl);
+		
+		System.out.println(transform.transform());
+		
+	}
 	
 	
 	
