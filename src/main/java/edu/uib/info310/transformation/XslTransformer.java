ae20f7b4e0fa90dbaf11862d038e769ad8ec42d0
EivindEE <eivind.elseth@gmail.com>
2011-11-02 09:26:03 +0100

    Added Javadoc to the XslTransformer class

--- src/main/java/edu/uib/info310/transformation/XslTransformer.java
+++ src/main/java/edu/uib/info310/transformation/XslTransformer.java
@@ -17,7 +17,7 @@ public class XslTransformer {
 
 	private Source xml;
 	private Source xsl;
-	
+
 	/**
 	 * Transforms an xml using the instance's xml and xsl sources
 	 * @return and outputStream with the transformed document
@@ -26,37 +26,61 @@ public class XslTransformer {
 	public OutputStream transform() throws TransformerException{
 		OutputStream out = new ByteArrayOutputStream();
 		Result result = new StreamResult(out);
-		
+
 		TransformerFactory factory = TransformerFactory.newInstance();
 		Transformer transformer = factory.newTransformer(xsl);
 		transformer.transform(xml, result);
-		
+
 		return out;
 	}
-	
-    
+
+	/**
+	 * Returns the Source containing the xml
+	 * @return
+	 */
 	public Source getXml() {
 		return xml;
 	}
 
+	/**
+	 * Sets the current xml source to be the provided xml source
+	 * @param xml
+	 */
 	public void setXml(Source xml) {
 		this.xml = xml;
 	}
-	
+
+	/**
+	 * Sets the current xml source to be a new source created with the provided xml file
+	 * @param xml
+	 */
 	public void setXml(File xml) {
 		this.xml = new StreamSource(xml);
 	}
 
+
+
+	/**
+	 * Returns the Source containing the xsl
+	 * @return
+	 */
 	public Source getXsl() {
 		return xsl;
 	}
 
+	/**
+	 * Sets the current xsl source to be the provided xsl source
+	 * @param xml
+	 */
 	public void setXsl(Source xsl) {
 		this.xsl = xsl;
 	}
-	
+	/**
+	 * Sets the current xsl source to be a new source created with the provided xsl file
+	 * @param xml
+	 */
 	public void setXsl(File xsl) {
 		this.xsl = new StreamSource(xsl);
 	}
-	
+
 }
