590d38199bbeb1cfeb20f71d57ecf21cef43ac76
Thomas Rasmussen <RasThomas@gmail.com>
2011-11-07 16:53:35 +0100

    Fixed encoding on correctArtist, need exception handling for failed connetion.

--- src/main/java/edu/uib/info310/search/LastFMSearch.java
+++ src/main/java/edu/uib/info310/search/LastFMSearch.java
@@ -2,6 +2,7 @@ package edu.uib.info310.search;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
@@ -30,18 +31,34 @@ public class LastFMSearch {
 	private static final String apiKey = "&api_key=a7123248beb0bbcb90a2e3a9ced3bee9";
 	private static final Logger LOGGER = LoggerFactory.getLogger(LastFMSearch.class);
 	
-	public InputStream artistCorrection(String search_string) throws Exception {
-		
+	public InputStream artistCorrection(String search_string)  {
+	    try{ 
+
 		URL lastFMRequest = new URL(artistCorrection + search_string + apiKey);
+		System.out.println(lastFMRequest);
         URLConnection lastFMConnection = lastFMRequest.openConnection();
+		System.out.println("Test1");
 		return lastFMConnection.getInputStream();
+    } catch(IOException ioexc){ 
+        System.out.println("Unavailable: "+ioexc.getMessage()); 
+    }
+		return null; 
+
 	}
 	
 	private Document docBuilder(String artist) throws Exception{
 	    DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
 	    domFactory.setNamespaceAware(true);
 	    DocumentBuilder builder = domFactory.newDocumentBuilder();
-	    Document doc = builder.parse(artistCorrection(artist));  
+		Document doc = null;
+		String safe_search = "";
+		try {
+			safe_search = URLEncoder.encode(artist, "UTF-8");
+		} catch (UnsupportedEncodingException e) {/*ignore*/}
+	    doc = builder.parse(artistCorrection(safe_search));  
+
+
+		System.out.println("Test2");
 	    return doc;
 }
 	public String correctArtist(String artist) throws Exception {
@@ -88,7 +105,7 @@ public class LastFMSearch {
 		FileOutputStream fileOutputStream = new FileOutputStream(file);
 		transform.transform().writeTo(fileOutputStream);
 //		System.out.println(transform.transform());
-		System.out.println(search.correctArtist("beatles"));
+		System.out.println(search.correctArtist("røyksopp"));
 		
 	}
 	
