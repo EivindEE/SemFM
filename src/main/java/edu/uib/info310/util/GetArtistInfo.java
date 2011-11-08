6d8fefb96487a26b0f170b0c872fd6df01489c2a
EivindEE <eivind.elseth@gmail.com>
2011-11-08 17:24:51 +0100

    Escaped ' in query. Added logging.

--- src/main/java/edu/uib/info310/util/GetArtistInfo.java
+++ src/main/java/edu/uib/info310/util/GetArtistInfo.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
@@ -20,52 +23,56 @@ import edu.uib.info310.sparql.QueryEndPointImp;
 
 
 public abstract class GetArtistInfo implements QueryEndPoint {
-	
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(GetArtistInfo.class);
 	public static String prefix = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
 			"PREFIX foaf: <http://xmlns.com/foaf/0.1/>" +
 			"PREFIX mo: <http://purl.org/ontology/mo/>" +
 			"PREFIX dbpedia: <http://dbpedia.org/property/>" +
 			"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
 			"PREFIX owl: <http://www.w3.org/2002/07/owl#>";
-	
+
 	public static Model ArtistInfo(String artistName){
 		String queryString;
 
-	 		if(artistName.isEmpty()){
-	 			System.out.println("No artist selected");
-	 			return null;
-	 		}
-	 	else {
-	 		QueryEndPoint qep = new QueryEndPointImp();
-	 		String query = "DESCRIBE *  WHERE { " +
- 					"?artist foaf:name '"+ artistName + "'. " +
-	 				" }";
-	 		qep.setQuery(prefix + query);
-	 		qep.setEndPoint(QueryEndPoint.BBC_MUSIC);
-	 		
-	 		Model rm = qep.describeStatement();
-	 		//ResultSetFormatter.out(rs);
-	 		
-	 		return rm;
-	 	     }				
-	 	}
-	
+		if(artistName.isEmpty()){
+			System.out.println("No artist selected");
+			return null;
+		}
+		else {
+			QueryEndPoint qep = new QueryEndPointImp();
+			String query = "DESCRIBE *  WHERE { " +
+					"?artist foaf:name \""+ artistName + "\". " +
+					" }";
+			qep.setQuery(prefix + query);
+			qep.setEndPoint(QueryEndPoint.BBC_MUSIC);
+
+			Model rm = qep.describeStatement();
+			//ResultSetFormatter.out(rs);
+
+			return rm;
+		}				
+	}
+
 	public static Model DbPediaArtistInfo(String artistName) throws Exception{
-	 		if(artistName.isEmpty()){
-	 			throw new Exception("artistName cannot be empty string in GetArtistInfo.DbPediaArtistInfo");
-	 		}
-	 	else {
-	 		QueryEndPoint qep = new QueryEndPointImp();
-	 		String query = "DESCRIBE *  WHERE { " +
- 					"?artist foaf:name '"+ artistName + "'. " +
-	 				" }";
-	 		qep.setQuery(prefix + query);
-	 		qep.setEndPoint(QueryEndPoint.DB_PEDIA);
-	 		
-	 		return qep.describeStatement();
-	 	     }				
-	 	}
-	
+		if(artistName.isEmpty()){
+			throw new Exception("artistName cannot be empty string in GetArtistInfo.DbPediaArtistInfo");
+		}
+		else {
+			QueryEndPoint qep = new QueryEndPointImp();
+			String queryStr = "DESCRIBE *  WHERE { " +
+					"?artist foaf:name \""+ artistName + "\"@en. " +
+					" }";
+			qep.setQuery(prefix + queryStr);
+			qep.setEndPoint(QueryEndPoint.DB_PEDIA);
+			Model model = qep.describeStatement();
+			LOGGER.debug("DBPedia search found " + model.size() + " statements" );
+			FileOutputStream out = new FileOutputStream(new File("log/dbpediaout.ttl"));
+			model.write(out, "TURTLE");
+			return model;
+		}				
+	}
+
 	public static void main(String[] args) throws FileNotFoundException{
 		System.out.println("started query");
 		Model test = ArtistInfo("Moby");
