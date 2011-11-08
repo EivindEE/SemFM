09acdfac9f4f2d4b9234113375801a5922223c83
Torstein Thune <torstein@gmail.com>
2011-11-08 15:53:52 +0100

    Added itunesLink to MockRecord

--- src/main/java/edu/uib/info310/model/mock/MockRecord.java
+++ src/main/java/edu/uib/info310/model/mock/MockRecord.java
@@ -41,7 +41,9 @@ public class MockRecord implements Record {
 	public String getId() {
 		return "http://dbpedia.org/resource/Music_of_the_Sun";
 	}
-	
+	public String getItunesLink() {
+		return "http://itunes.apple.com/";
+	}
 	public String getDescription() {
 		return "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 	}
