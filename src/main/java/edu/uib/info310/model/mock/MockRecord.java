fa3c64f778c3ccb94c9fcb60d438a0f181283a7b
Torstein Thune <torstein@gmail.com>
2011-11-08 15:31:46 +0100

    added new elements to mock classes

--- src/main/java/edu/uib/info310/model/mock/MockRecord.java
+++ src/main/java/edu/uib/info310/model/mock/MockRecord.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import edu.uib.info310.model.Artist;
 import edu.uib.info310.model.Record;
 import edu.uib.info310.model.Track;
 
@@ -39,6 +40,29 @@ public class MockRecord implements Record {
 
 	public String getId() {
 		return "http://dbpedia.org/resource/Music_of_the_Sun";
+	}
+	
+	public String getDescription() {
+		return "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+	}
+	
+	public List<Artist> getArtist(){
+		List<Artist> artists = new LinkedList<Artist>();
+		artists.add(new MockArtist());
+		return artists;
+	}
+	
+	public List<Record> getRelatedRecord(){
+		List<Record> records = new LinkedList<Record>();
+		records.add(new MockRecord());
+		records.add(new MockRecord());
+		records.add(new MockRecord());
+		records.add(new MockRecord());
+		records.add(new MockRecord());
+		records.add(new MockRecord());
+		records.add(new MockRecord());
+		records.add(new MockRecord());
+		return records;
 	}
 
 }
