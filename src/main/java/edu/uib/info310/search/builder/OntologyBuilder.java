ac8cb9e9ec3cfe1776b29ccf47e41452c9cf91ba
EivindEE <eivind.elseth@gmail.com>
2011-11-08 15:20:06 +0100

    Updated OntologyBuilder to use getCorrectName() from LastFMSearch.

--- src/main/java/edu/uib/info310/search/builder/OntologyBuilder.java
+++ src/main/java/edu/uib/info310/search/builder/OntologyBuilder.java
@@ -15,6 +15,7 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.reasoner.Reasoner;
 import com.hp.hpl.jena.reasoner.ReasonerRegistry;
 
+import edu.uib.info310.search.ArtistNotFoundException;
 import edu.uib.info310.search.DiscogSearch;
 import edu.uib.info310.search.ITunesSearcher;
 import edu.uib.info310.search.LastFMSearch;
@@ -33,33 +34,26 @@ public class OntologyBuilder {
 	private static final String SIMILAR_XSL = "src/main/resources/XSL/SimilarArtistLastFM.xsl";
 	private static final String ARTIST_EVENTS_XSL = "src/main/resources/XSL/Events.xsl";
 	private static final Logger LOGGER = LoggerFactory.getLogger(OntologyBuilder.class);
-	private DiscogSearch disc = new DiscogSearch();
+//	private DiscogSearch disc = new DiscogSearch();
 	private ITunesSearcher itunes = new ITunesSearcher();
-	public Model createArtistOntology(String search_string) {
+	public Model createArtistOntology(String search_string) throws ArtistNotFoundException {
 		Model model = ModelFactory.createDefaultModel();
-//		Model discs = disc.getDiscography(search_string); 
-//		LOGGER.debug("Number of items in discography: " + discs.size());
-//		model.add(discs);
-//		LOGGER.debug("Model size after getting artist discography: " + model.size());
-//		Model tracks = disc.getTracks(search_string); 
-//		LOGGER.debug("Number of tracks to discography: " + tracks.size());
-//		model.add(tracks);
-//		LOGGER.debug("Model size after getting artist tracks: " + model.size());
+		String correctName = search.correctArtist(search_string);
 		try{
-			transformer.setXml(search.getSimilarArtist(search_string));
+			transformer.setXml(search.getSimilarArtist(correctName));
 			transformer.setXsl(new File(SIMILAR_XSL));
 
 			InputStream in = new ByteArrayInputStream(transformer.transform().toByteArray());
 			model.read(in, null);
 			LOGGER.debug("Model size after getting similar artists: " + model.size());
-			transformer.setXml(search.getArtistEvents(search_string));
+			transformer.setXml(search.getArtistEvents(correctName));
 			transformer.setXsl(new File(ARTIST_EVENTS_XSL));
 
 			in = new ByteArrayInputStream(transformer.transform().toByteArray());
 			model.read(in, null);
 			LOGGER.debug("Model size after getting artist events: " + model.size());
 			
-			model.add(itunes.getRecords(search_string));
+			model.add(itunes.getRecords(correctName));
 			LOGGER.debug("Model size after adding record info from iTunes: " + model.size());
 		} 
 		catch (Exception e) {
@@ -68,14 +62,11 @@ public class OntologyBuilder {
 
 		// get BBC_MUSIC & DB_PEDIA model and add to model
 		try {
-			model.add(GetArtistInfo.ArtistInfo(search_string));
+			model.add(GetArtistInfo.ArtistInfo(correctName));
 			LOGGER.debug("Finished BBC");
-			model.add(GetArtistInfo.DbPediaArtistInfo(search_string));
+			model.add(GetArtistInfo.DbPediaArtistInfo(correctName));
 			LOGGER.debug("Finished DBPedia");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-//			e.printStackTrace();
-		}
+		} catch (Exception e) {}
 		
 		String queryStr = "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX mo:<http://purl.org/ontology/mo/> PREFIX foaf:<http://xmlns.com/foaf/0.1/> PREFIX owl:<http://www.w3.org/2002/07/owl#> " +
 				"	CONSTRUCT {?artist owl:sameAs ?artist2.} " +
