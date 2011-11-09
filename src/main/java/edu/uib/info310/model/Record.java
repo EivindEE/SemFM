fd58a22649ce42e5225e11ed50d46994f501855f
EivindEE <eivind.elseth@gmail.com>
2011-11-09 14:48:58 +0100

    Modified methods available in Record and implementations.

--- src/main/java/edu/uib/info310/model/Record.java
+++ src/main/java/edu/uib/info310/model/Record.java
@@ -1,6 +1,7 @@
 package edu.uib.info310.model;
 
 import java.util.List;
+import java.util.Map;
 
 
 public interface Record {
@@ -11,20 +12,26 @@ public interface Record {
 	
 	public String getImage();
 	
-	public String getYear();
-	
-	public String getLabel();
-	
 	public String getDescription();
 	
 	public String getItunesLink();
-	
-	public String getPlaytime();
 	
 	public List<Artist> getArtist();
 	
 	public List<Track> getTracks();
 	
 	public List<Record> getRelatedRecords();
+	
+	public List<String> getReviews();
+	
+	public List<String> getGenres();
+	
+	public String getSpotifyUri();
+	
+	public Map<String,Object> getMeta();
+	
+	public String getItunesPreview();
+	
+	
 
 }
