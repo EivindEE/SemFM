803f93e590e1d6800880e0b418619282a54286a8
John Fredrik <John Fredrik@JohnFredrik-PC>
2011-11-11 21:47:30 +0100

    Made a request from Discogs for Album info and translatet via XSLT to RDF and added to model

---
+++ src/main/java/edu/uib/info310/search/MasterNotFoundException.java
@@ -0,0 +1,9 @@
+package edu.uib.info310.search;
+
+public class MasterNotFoundException extends Exception {
+	
+	public MasterNotFoundException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
