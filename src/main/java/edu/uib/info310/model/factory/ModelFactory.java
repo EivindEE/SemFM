da347765c6c3f6f00cf38dd08d65bbf29e9b856d
EivindEE <eivind.elseth@gmail.com>
2011-11-14 11:35:00 +0100

    Added a interface ModelFactory, and a implemtentation ModelFactoryImpl. These should be used to create new instances of Artist, Record, Event, and Track instead of using 'new'.

---
+++ src/main/java/edu/uib/info310/model/factory/ModelFactory.java
@@ -0,0 +1,21 @@
+package edu.uib.info310.model.factory;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+import edu.uib.info310.model.Artist;
+import edu.uib.info310.model.Event;
+import edu.uib.info310.model.Record;
+import edu.uib.info310.model.Track;
+
+@Controller
+public interface ModelFactory {
+	
+	public Artist createArtist();
+	
+	public Record createRecord();
+	
+	public Track createTrack();
+	
+	public Event createEvent();
+}
