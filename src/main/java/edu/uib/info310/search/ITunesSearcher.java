a6a11381aecc269bf9df648ab96a7a49ab8eef60
EivindEE <eivind.elseth@gmail.com>
2011-11-16 14:36:00 +0100

    Added track info to the model in iTunesSearcher.

--- src/main/java/edu/uib/info310/search/ITunesSearcher.java
+++ src/main/java/edu/uib/info310/search/ITunesSearcher.java
@@ -11,6 +11,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.sql.Time;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -29,6 +30,7 @@ import com.hp.hpl.jena.sparql.vocabulary.FOAF;
 import com.hp.hpl.jena.vocabulary.DCTerms;
 import com.hp.hpl.jena.vocabulary.OWL;
 import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
 
 import edu.uib.info310.vocabulary.MO;
 
@@ -36,18 +38,19 @@ import edu.uib.info310.vocabulary.MO;
 
 @Component
 public class ITunesSearcher {
-	private static String DEFAULT_URL = "http://itunes.apple.com/search?entity=album&limit=200&country=NO&term=";
+	private static String ALBUM = "http://itunes.apple.com/search?entity=album&limit=200&country=NO&term=";
+	private static String TRACK = "http://itunes.apple.com/search?entity=musicTrack&limit=1000&country=NO&term=";
 	private static String SINGLE = " - Single";
 	private static String EP = " - EP";
 	private static final Logger LOGGER = LoggerFactory.getLogger(ITunesSearcher.class);
 
-	public Model getRecords(String artist, String artistUri){
+	public Model getRecordsByArtistName(String artist, String artistUri){
 		Model model = ModelFactory.createDefaultModel();
 		Resource subject;
 		Property property;
 		RDFNode  rdfObject;
 		try {
-			URL url = new URL((DEFAULT_URL + URLEncoder.encode(artist, "UTF-8")));
+			URL url = new URL((ALBUM + URLEncoder.encode(artist, "UTF-8")));
 			URLConnection connection = url.openConnection();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
@@ -68,15 +71,15 @@ public class ITunesSearcher {
 						collectionName = collectionName.substring(0, collectionName.length() - SINGLE.length());
 					else if(collectionName.endsWith(EP))
 						collectionName = collectionName.substring(0, collectionName.length() - EP.length());
-					
+
 					property = DCTerms.title;
 					rdfObject = model.createResource(collectionName);
 					model.add(subject, property, rdfObject);
-					
+
 					property = RDF.type;
 					rdfObject = MO.Record;
 					model.add(subject, property, rdfObject);
-					
+
 					property = DCTerms.issued;
 					rdfObject = model.createLiteral(jObject.get("releaseDate").toString());
 					model.add(subject, property, rdfObject);
@@ -88,12 +91,12 @@ public class ITunesSearcher {
 					property = FOAF.maker;
 					rdfObject = model.createResource(artistUri);
 					model.add(subject, property, rdfObject);
-					
+
 					subject = model.createResource(artistUri);
 					property = FOAF.made;
 					rdfObject = model.createResource(jObject.get("collectionViewUrl").toString());
 					model.add(subject, property, rdfObject);
-					
+
 					property = OWL.sameAs;
 					rdfObject = model.createResource(jObject.get("artistViewUrl").toString());
 					model.add(subject, property, rdfObject);
@@ -112,11 +115,105 @@ public class ITunesSearcher {
 			FileOutputStream out = new FileOutputStream(new File("log/iTunesOut.ttl"));
 			model.write(out, "TURTLE");
 		} catch (FileNotFoundException e) {	LOGGER.error("Got FileNotFoundException: " + e.toString());	}
-		
+
 		if(model.isEmpty()){
 			try {
-				LOGGER.debug("Got 0 results for query: " + DEFAULT_URL + URLEncoder.encode(artist, "UTF-8"));
+				LOGGER.debug("Got 0 results for query: " + ALBUM + URLEncoder.encode(artist, "UTF-8"));
 			} catch (UnsupportedEncodingException e) {			}
+		}
+		return model;
+	}
+
+	public Model getRecordWithNameAndArtist(String albumUri, String album, String artist){
+		Model model = ModelFactory.createDefaultModel();
+		Resource subject;
+		Property property;
+		RDFNode  rdfObject;
+		try {
+			URL url = new URL((ALBUM + URLEncoder.encode(artist, "UTF-8")));
+			URLConnection connection = url.openConnection();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+			JSONParser parser = new JSONParser();
+			Object obj = parser.parse(reader);
+			JSONObject response = (JSONObject) obj;
+			JSONArray results = (JSONArray) response.get("results");
+			for (Object object : results) {
+				JSONObject jObject = (JSONObject) object;
+				String iTunesArtist = jObject.get("artistName").toString().toLowerCase();
+				String iTunesAlbum = jObject.get("collectionName").toString().toLowerCase();
+				if(iTunesArtist.equals(artist.toLowerCase()) && iTunesAlbum.equals(album.toLowerCase())){
+					subject = model.createResource(albumUri);
+					property = MO.image;
+					rdfObject = model.createResource(jObject.get("artworkUrl100").toString());
+					model.add(subject, property, rdfObject);
+					
+					property = MO.genre;
+					rdfObject = model.createLiteral(jObject.get("primaryGenreName").toString());
+					model.add(subject, property, rdfObject);
+					
+					property = OWL.sameAs;
+					rdfObject = model.createResource(jObject.get("collectionViewUrl").toString());
+					model.add(subject, property, rdfObject);
+				}
+				
+			}
+			
+			url = new URL((TRACK + URLEncoder.encode(artist + " " + album, "UTF-8")));
+			connection = url.openConnection();
+			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+			parser = new JSONParser();
+			obj = parser.parse(reader);
+			response = (JSONObject) obj;
+			results = (JSONArray) response.get("results");
+			for (Object object : results) {
+				JSONObject jObject = (JSONObject) object;
+				String iTunesArtist = jObject.get("artistName").toString().toLowerCase();
+				String iTunesAlbum = jObject.get("collectionName").toString().toLowerCase();
+				if(iTunesArtist.equals(artist.toLowerCase()) && iTunesAlbum.equals(album.toLowerCase())){
+					String trackUri = albumUri + "/track/" + jObject.get("trackNumber").toString();
+					subject = model.createResource(albumUri);
+					property = MO.track;
+					rdfObject = model.createResource(trackUri);
+					model.add(subject, property, rdfObject);
+					
+					subject = model.createResource(trackUri);
+					property = MO.preview;
+					rdfObject = model.createLiteral(jObject.get("previewUrl").toString());
+					model.add(subject, property, rdfObject);
+					
+					property = MO.track_number;
+					rdfObject = model.createLiteral(jObject.get("trackNumber").toString());
+					model.add(subject, property, rdfObject);
+					
+					property = RDF.type;
+					rdfObject = MO.Track;
+					model.add(subject, property, rdfObject);
+					
+					property = model.createProperty("http://www.w3.org/2006/time#duration");
+					long timeMillis = Long.parseLong(jObject.get("trackTimeMillis").toString());
+					long time = timeMillis / 1000;  
+					String seconds = Integer.toString((int)(time % 60));  
+					String minutes = Integer.toString((int)((time % 3600) / 60));  
+					for (int i = 0; i < 2; i++) {  
+						if (seconds.length() < 2) {  
+						seconds = "0" + seconds;  
+						}
+					}
+					rdfObject = model.createLiteral(minutes + ":" + seconds);  
+					model.add(subject, property, rdfObject);
+					
+					property = FOAF.name;
+					rdfObject = model.createLiteral(jObject.get("trackName").toString());
+					model.add(subject, property, rdfObject);
+				}
+				
+			}
+			
+		}
+		catch (Exception e) {
+			LOGGER.error("Caught an exception: " + e.toString());
 		}
 		return model;
 	}
