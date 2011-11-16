839c6fceee62cb9fd6019af061f1557d372dfe56
EivindEE <eivind.elseth@gmail.com>
2011-11-16 10:52:31 +0100

    Added equals and hashCode methods.

--- src/main/java/edu/uib/info310/model/imp/RecordImp.java
+++ src/main/java/edu/uib/info310/model/imp/RecordImp.java
@@ -166,4 +166,37 @@ public class RecordImp implements Record{
 	public void setTracks(List<Track> tracks) {
 		this.tracks = tracks;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((artist == null) ? 0 : artist.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RecordImp other = (RecordImp) obj;
+		if (artist == null) {
+			if (other.artist != null)
+				return false;
+		} else if (!artist.equals(other.artist))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+
 }
