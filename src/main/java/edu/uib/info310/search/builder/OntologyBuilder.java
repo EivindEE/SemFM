9949e6f94466a5c41c8da4d4521286b3857d9186
EivindEE <eivind.elseth@gmail.com>
2011-11-17 11:37:20 +0100

    Removed failed attempts to correct the encoding error.

--- src/main/java/edu/uib/info310/search/builder/OntologyBuilder.java
+++ src/main/java/edu/uib/info310/search/builder/OntologyBuilder.java
@@ -6,12 +6,16 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
 
 import javax.xml.transform.TransformerException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 import com.hp.hpl.jena.query.QueryExecution;
@@ -37,10 +41,10 @@ public class OntologyBuilder {
 
 	@Autowired
 	private XslTransformer transformer;
-	
+
 	@Autowired
 	private ITunesSearcher itunes;
-	
+
 	@Autowired
 	private DiscogSearch discog;
 
@@ -48,16 +52,17 @@ public class OntologyBuilder {
 	private static final String ARTIST_EVENTS_XSL = "src/main/resources/XSL/Events.xsl";
 	private static final Logger LOGGER = LoggerFactory.getLogger(OntologyBuilder.class);
 
-	
+
 	public Model createArtistOntology(String search_string) throws ArtistNotFoundException {
 		Model model = ModelFactory.createDefaultModel();
 		String correctName = search.correctArtist(search_string);
-		
+
 		try {
 			transformer.setXml(search.getSimilarArtist(correctName));
 			transformer.setXsl(new File(SIMILAR_XSL));
 
 			InputStream in = new ByteArrayInputStream(transformer.transform().toByteArray());
+
 			model.read(in, null);
 			LOGGER.debug("Model size after getting similar artists: " + model.size());
 			transformer.setXml(search.getArtistEvents(correctName));
@@ -69,25 +74,26 @@ public class OntologyBuilder {
 		} catch(Exception e) {
 			LOGGER.error("Threw exception" + e);
 		}
+
 		String id = "";
 		String artistName = "";
 		String getIdQuery = "PREFIX foaf:<http://xmlns.com/foaf/0.1/> PREFIX mo:<http://purl.org/ontology/mo/> SELECT ?id ?name WHERE {?id mo:similar-to ?something; foaf:name ?name . }";
 		QueryExecution exec = QueryExecutionFactory.create(getIdQuery, model);
 		ResultSet ids = exec.execSelect();
 		if(ids.hasNext()){
-			QuerySolution sol =ids.next(); 
-			id = sol.get("id").toString();
-			artistName = sol.get("name").toString();
+			QuerySolution sol = ids.next(); 
+			id = sol.get("id").asResource().getURI();
+			artistName = sol.get("name").asLiteral().getString();
 			LOGGER.debug("Sending id:" + id + ", and name: " + artistName + "  to iTunes, BBC and DBPedia");
 		}else{
 			LOGGER.debug("Did not find id for artist with name " + correctName);
 		}
 
-		if(artistName.isEmpty() || artistName==null || model.size() < 3) {
+		if(artistName.isEmpty() || artistName == null || model.size() < 3) {
 			LOGGER.debug("Artist not found");
 			throw new ArtistNotFoundException("Last.FM found name, but no data.");
 		}
-		
+
 		try{
 			model.add(itunes.getRecordsByArtistName(artistName, id));
 			LOGGER.debug("Model size after adding record info from iTunes: " + model.size());
@@ -96,7 +102,7 @@ public class OntologyBuilder {
 			LOGGER.debug("Model size after adding artist info from BBC: " + model.size());
 
 			model.add(GetArtistInfo.DBPedia(artistName, id));
-			LOGGER.debug("Model size after adding artis info from DBPedia: " + model.size());
+			LOGGER.debug("Model size after adding artist info from DBPedia: " + model.size());
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
@@ -113,46 +119,50 @@ public class OntologyBuilder {
 
 		return  model;
 	}
-	
+
 	public Model createRecordOntology(String releaseId, String record_name, String artist_name){
-			
-			LOGGER.debug("Got releaseID: " + releaseId);
-			File xsl = new File("src/main/resources/XSL/AlbumXSLT.xsl");
-				
-			XslTransformer transform = new XslTransformer();
-			
-			try {
-				transform.setXml(discog.getAlbumURI(releaseId));
-			} catch (MasterNotFoundException e1) {
-				LOGGER.error(e1.toString());
-			}
-			transform.setXsl(xsl);
-			
-			Model model = ModelFactory.createDefaultModel();
-			InputStream in = null;
-			try {
-				in = new ByteArrayInputStream(transform.transform().toByteArray());
-			} catch (TransformerException e1) {
-				LOGGER.error(e1.toString());
-			}
-			model.read(in,null);
-			LOGGER.debug("Model size after getting album info: " + model.size());
-			
-			
-			String albumUri = "http://api.discogs.com/release/" + releaseId;
-			Model itunesModel = itunes.getRecordWithNameAndArtist(albumUri, record_name, artist_name);
-			model.add(itunesModel);
-			LOGGER.debug("Model size after getting iTunes info: " + model.size());
-			try {
-				FileOutputStream out = new FileOutputStream(new File("log/albumout.ttl"));
-				model.write(out,"TURTLE");
-			} catch (FileNotFoundException e) {
-				LOGGER.error(e.toString());
-			}
-			
-			return model;
-		
-		
+
+		LOGGER.debug("Got releaseID: " + releaseId);
+		File xsl = new File("src/main/resources/XSL/AlbumXSLT.xsl");
+
+		XslTransformer transform = new XslTransformer();
+
+		try {
+			transform.setXml(discog.getAlbumURI(releaseId));
+		} catch (MasterNotFoundException e1) {
+			LOGGER.error(e1.toString());
+		}
+		transform.setXsl(xsl);
+
+		Model model = ModelFactory.createDefaultModel();
+		InputStream in = null;
+		try {
+			in = new ByteArrayInputStream(transform.transform().toByteArray());
+		} catch (TransformerException e1) {
+			LOGGER.error(e1.toString());
+		}
+		model.read(in,null);
+		LOGGER.debug("Model size after getting album info: " + model.size());
+
+
+		String albumUri = "http://api.discogs.com/release/" + releaseId;
+		Model itunesModel = itunes.getRecordWithNameAndArtist(albumUri, record_name, artist_name);
+		model.add(itunesModel);
+		LOGGER.debug("Model size after getting iTunes info: " + model.size());
+		try {
+			FileOutputStream out = new FileOutputStream(new File("log/albumout.ttl"));
+			model.write(out,"TURTLE");
+		} catch (FileNotFoundException e) {
+			LOGGER.error(e.toString());
+		}
+
+		return model;
+	}
+
+	public static void main(String[] args) throws ArtistNotFoundException {
+		ApplicationContext context = new ClassPathXmlApplicationContext("main-context.xml");
+		OntologyBuilder builder = (OntologyBuilder) context.getBean("ontologyBuilder");
+		builder.createArtistOntology("Bjšrk");
 	}
 
 }
