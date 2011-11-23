111a405980ffaa31bff3bc64a0274827771ea0a9
EivindEE <eivind.elseth@gmail.com>
2011-11-23 12:08:44 +0100

    Removed unused methods from DiscogsOntology

--- src/main/java/edu/uib/info310/search/builder/ontology/DiscogOntology.java
+++ src/main/java/edu/uib/info310/search/builder/ontology/DiscogOntology.java
@@ -8,14 +8,6 @@ import edu.uib.info310.exception.MasterNotFoundException;
 
 public interface DiscogOntology {
 
-	public Model getDiscography(String search_string);
-
-	public Model getTracks(String search_string);
-
-	public Model getAlbum(String search_string);
-
-	public Model getAlbums(String search_string);
-
 	public InputStream getAlbumURI(String releaseId)
 			throws MasterNotFoundException;
 
--- src/main/java/edu/uib/info310/search/builder/ontology/impl/DiscogOntologyImpl.java
+++ src/main/java/edu/uib/info310/search/builder/ontology/impl/DiscogOntologyImpl.java
@@ -2,10 +2,8 @@ package edu.uib.info310.search.builder.ontology.impl;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.URLEncoder;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -23,7 +21,6 @@ import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.sparql.engine.http.QueryEngineHTTP;
 
 import edu.uib.info310.exception.MasterNotFoundException;
@@ -43,122 +40,6 @@ public class DiscogOntologyImpl implements DiscogOntology {
 					"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>" +
 					"PREFIX dc: <http://purl.org/dc/terms/>" +
 					"PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>";
-
-
-	/* (non-Javadoc)
-	 * @see edu.uib.info310.search.builder.ontology.DiscogOntology#getDiscography(java.lang.String)
-	 */
-	public Model getDiscography(String search_string){
-		String safe_search = "";
-		try {
-			safe_search = URLEncoder.encode(search_string, "UTF-8");
-		} catch (UnsupportedEncodingException e) {/*ignore*/}
-
-		String searchString =" PREFIX INPUT: <http://www.discogs.com/artist/" + safe_search + ">" +
-				"DESCRIBE * where{ ?artist1 mo:discogs INPUT:." +
-				"?artist1 foaf:made ?allReleases. ?allReleases rdf:type mo:Record.}";
-
-		Query query = QueryFactory.create(PREFIX + searchString);
-		QueryEngineHTTP queryExecution = QueryExecutionFactory.createServiceRequest("http://api.kasabi.com/dataset/discogs/apis/sparql", query);
-
-		queryExecution.addParam("apikey", "fe29b8c58180640f6db16b9cd3bce37c872c2036");
-
-
-
-		return queryExecution.execDescribe();
-	}
-	/* (non-Javadoc)
-	 * @see edu.uib.info310.search.builder.ontology.DiscogOntology#getTracks(java.lang.String)
-	 */
-	public Model getTracks(String search_string){
-		String safe_search = "";
-		try {
-			safe_search = URLEncoder.encode(search_string, "UTF-8");
-		} catch (UnsupportedEncodingException e) {/*ignore*/}
-
-		String searchString =" PREFIX INPUT: <http://data.kasabi.com/dataset/discogs/artist/" + safe_search.toLowerCase().replace("+", "-") + ">" +
-
-	 			"DESCRIBE * where{ ?release foaf:maker INPUT:." +
-	 			"?release rdf:type mo:Track.}";
-
-		Query query = QueryFactory.create(PREFIX + searchString);
-		QueryEngineHTTP queryExecution = QueryExecutionFactory.createServiceRequest("http://api.kasabi.com/dataset/discogs/apis/sparql", query);
-
-		queryExecution.addParam("apikey", "fe29b8c58180640f6db16b9cd3bce37c872c2036");
-
-		return queryExecution.execDescribe();
-	}
-
-
-	/* (non-Javadoc)
-	 * @see edu.uib.info310.search.builder.ontology.DiscogOntology#getAlbum(java.lang.String)
-	 */
-	public Model getAlbum(String search_string){
-		String constructStr = "CONSTRUCT {?artist foaf:made ?record;" +
-				"foaf:name ?artistName;" +
-				"rdf:type mo:MusicArtist ." +
-				"?record rdfs:comment ?comment;" +
-				"dc:issued ?issued;" +
-				"rdf:type mo:Record;" +
-				"mo:discogs ?discogs;" +
-				"foaf:name ?recordName;" +
-				"mo:publisher ?publisher;" +
-				"mo:track ?track";
-
-
-		String whereStr = "} WHERE {?record dc:title \""+ search_string + "\" ;" +
-				"rdf:type mo:Record;" +
-				"foaf:maker ?artist;" +
-				"dc:issued ?issued;" +
-				"mo:discogs ?discogs;" +
-				"dc:title ?recordName;" +
-				"mo:publisher ?publisher;" +
-				"mo:track ?track." +
-				"?artist foaf:name ?artistName}";
-
-
-		Query query = QueryFactory.create(PREFIX + constructStr + whereStr);
-		QueryEngineHTTP queryExecution = QueryExecutionFactory.createServiceRequest("http://api.kasabi.com/dataset/discogs/apis/sparql", query);
-		queryExecution.addParam("apikey", "fe29b8c58180640f6db16b9cd3bce37c872c2036");
-
-		return queryExecution.execConstruct();
-	}
-
-
-	/* (non-Javadoc)
-	 * @see edu.uib.info310.search.builder.ontology.DiscogOntology#getAlbums(java.lang.String)
-	 */
-	public Model getAlbums(String search_string){
-		String safe_search = "";
-		try {
-			safe_search = URLEncoder.encode(search_string, "UTF-8");
-		} catch (UnsupportedEncodingException e) {/*ignore*/}
-
-		String constructStr = "CONSTRUCT {?artist foaf:made ?record;" +
-				"foaf:name ?artistName;" +
-				"rdf:type mo:MusicArtist ." +
-				"?record rdf:type mo:Record;" +
-				"mo:discogs ?discogs;" +
-				"dc:title ?record.";
-
-
-		String whereStr = "} WHERE {?record dc:title \""+ safe_search + "\" ;" +
-				"rdf:type mo:Record;" +
-				"foaf:maker ?artist;" +
-				"mo:discogs ?discogs;" +
-				"dc:title ?recordName." +
-				"?artist foaf:name ?artistName}";
-
-
-		Query query = QueryFactory.create(PREFIX + constructStr + whereStr);
-		QueryEngineHTTP queryExecution = QueryExecutionFactory.createServiceRequest("http://api.kasabi.com/dataset/discogs/apis/sparql", query);
-
-		queryExecution.addParam("apikey", "fe29b8c58180640f6db16b9cd3bce37c872c2036");
-
-
-
-		return queryExecution.execConstruct();
-	}
 
 	private InputStream getAlbumInputStream(String uri, String releaseId) throws MasterNotFoundException{
 		InputStream in = null;
